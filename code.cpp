f3fed6dfaadd60fbb701538d7f1ed4a662999e64
Nasir1290 <nasirmia12901290@gmail.com>
2024-02-04 08:26:55 +0600

    add some code

--- code.cpp
+++ code.cpp
@@ -21,7 +21,6 @@
 //     return 0;
 // }
 
-
 // two different method for print hello world 👇👇
 
 // #include<iostream>
@@ -31,7 +30,6 @@
 //     return 0;
 // }
 
-
 // #include <cstdio>
 // using namespace std;
 // int main(){
@@ -39,8 +37,21 @@
 //     return 0;
 // }
 
-
 // return type in c++.. we have to return depends on our methosd declaration... like if we declar our
 // method with int , then we have to return 0,1 or any other integer
 // if we declare method with void , then we have to return nothing
 // if we declare method with char , then we have to return charecter like this 👇
+
+#include <iostream>
+using namespace std;
+
+void nothing();
+int main()
+{
+    nothing();
+    return 0;
+}
+void nothing()
+{
+    cout << "nothing" << endl;
+};
